9353a8becff3e0bd4bfd2735fccac55e3293fcff
Fabian Mora <fabianmoravelez6@gmail.com>
2024-02-28 13:09:32 -0500

    Agregue clip-path a cada contenedor de la imagen y con una funcion obtuve su posicion para agregarle una clase personalizada

--- js/app.js
+++ js/app.js
@@ -1,44 +1,36 @@
-var htmlCollection = document.getElementsByClassName('item');
-var image = document.getElementsByClassName('imagen-persona');
-var images = Array.from(image);
-var itemsId = Array.from(htmlCollection);
-var sectionDeg = 360/itemsId.length;//24              // Seccionar el círculo imaginario en un número igual al número de elementos del array
+var imagesCarousel = document.getElementsByClassName('item');
+var itemsId = Array.from(imagesCarousel);
+var sectionDeg = 360/itemsId.length;              // Seccionar el círculo imaginario en un número igual al número de elementos del array
 var radianSectionDeg = sectionDeg*Math.PI*2/360;  // Transformar de grados a radianes
 var radiusLength = 250;                           // Distancia entre el centro del cítuclo y el elemento  
 
 //Posicion de cada item
 for(var i=0; i<itemsId.length; i++){
-  // var angle = sectionDeg * i - 90;
   itemsId[i].style.top = radiusLength*Math.sin(radianSectionDeg*i-1.5708)+'px'; //1.5708 radianes son 90 grados
   itemsId[i].style.left = radiusLength*Math.cos(radianSectionDeg*i-1.5708)+'px';
-  // itemsId[i].style.transform = 'rotate(' + angle + 'deg)';
-  // var angle = sectionDeg * i - 90; // Ajuste para que la primera imagen comience en la parte superior
-  // var x = radiusLength * Math.cos(angle * Math.PI / 180);
-  // var y = radiusLength * Math.sin(angle * Math.PI / 180);
-
-  itemsId[i].style.top = y + 'px';
-  itemsId[i].style.left = x + 'px';
-  // itemsId[i].style.transform = 'rotate(' + angle + 'deg)';
+  checkPosition();
 }
-
-// for(var i=0; i<images.length; i++){
-//   var angle = sectionDeg * i - 180; // Ajuste para que la primera imagen comience en la parte superior
-//   images[i].style.transform = 'rotate(' + angle + 'deg)';
-// }
 
 // Función usada para establecer la nueva posición de los elementos
 function makeATurn(){
   for(var i=0; i<itemsId.length; i++){
- 
-    itemsId[i].style.top = radiusLength*Math.sin(radianSectionDeg*i-1.5708)+'px';
-    itemsId[i].style.left = radiusLength*Math.cos(radianSectionDeg*i-1.5708)+'px';
-
-  
+      itemsId[i].style.top = radiusLength*Math.sin(radianSectionDeg*i-1.5708)+'px';
+      itemsId[i].style.left = radiusLength*Math.cos(radianSectionDeg*i-1.5708)+'px';
     }
+    checkPosition();
 }
-function rotateItem(){
-  for(var i = 0; i<itemsId.length; i++){
-    itemsId[i].style.transform = 'rotate(45deg)';
+
+// Función para verificar la posición y anadir clase personalizada
+function checkPosition() {
+  for (var i = 0; i < itemsId.length; i++) {
+    // Remover todas las clases de posición primero
+    for (var j = 1; j <= 15; j++) {
+      itemsId[i].classList.remove('posicion-' + j);
+    }
+
+    // Calcular la posición y agregar la clase correspondiente
+    var posicion = (i + itemsId.length - 1) % itemsId.length + 1;
+    itemsId[i].classList.add('posicion-' + posicion);
   }
 }
 
@@ -47,7 +39,7 @@ function turnRight(){
   var holder = itemsId.pop();
   itemsId.unshift(holder);
   makeATurn();
-  rotateItem();
+  // rotateItem();
 }
 
 function turnLeft(){
